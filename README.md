dbe129b78cbc72327990e26211259430a7334112
Wahid Farid <wahid.farid@tablecheck.com>
2023-02-26 16:00:19 +0900

    initial mongo

---
+++ README.md
@@ -0,0 +1,10 @@
+### Checklist
+
+- [x] setup a mongo db
+- [x] connect website to mongo db
+- [ ] include basic wysiwyg editor
+- [ ] upload that to mongo db
+- [ ] read lists/details of nodes
+- [ ] edit/update nodes
+- [ ] graph nodes
+- [ ] label/categorize nodes
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+import * as Realm from "realm-web";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const app = new Realm.App({ id: import.meta.env.VITE_DB_ID });
+  const credentials = Realm.Credentials.emailPassword(
+    import.meta.env.VITE_USER_EMAIL,
+    import.meta.env.VITE_USER_PSWD
+  );
+
+  app.logIn(credentials).then((user) => {
+    // `App.currentUser` updates to match the logged in user
+    console.assert(user.id === app.currentUser.id);
+    console.log({ user, app });
+  });
 
   return (
     <div className="App">
@@ -28,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
